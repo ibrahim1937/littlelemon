5422a5ea77e4823a8e2646a876e2dcb47436ad8d
ibrahim1937 <ibrahimchahboune@gmail.com>
2023-07-19 11:23:04 +0100

    setting up booking api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from restaurant import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet, basename='tables')
--- restaurant/views.py
+++ restaurant/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render
 from restaurant.serializers import MenuSerializer, BookingSerializer
 from rest_framework.generics import ListCreateAPIView, DestroyAPIView, RetrieveUpdateAPIView
 from restaurant.models import Menu, Booking
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
-#MenuItemView – inheriting the rest_framework.generics.ListCreateView class. It handles the POST and GET method calls.
 class MenuItemView(ListCreateAPIView):
     serializer_class = MenuSerializer
 
     def get_queryset(self):
         return Menu.objects.all()
     
-
-
-#SingleMenuItemView – inherits the RetrieveUpdateAPIView and DestroyAPIView classes both imported from the rest_framework.generics module. This class is responsible for processing GET, PUT and DELETE method calls.
-
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     serializer_class = MenuSerializer
 
     def get_queryset(self):
         return Menu.objects.all()
+    
+class BookingViewSet(ModelViewSet):
+    serializer_class = BookingSerializer
+
+    def get_queryset(self):
+        return Booking.objects.all()
